5d92c375b8b0f0ba034658ba8c547efd08316083
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-01-16 09:53:53 +0100

    Update DNIsolucionSCORM.java

--- T5/untitled/src/DNIsolucionSCORM.java
+++ T5/untitled/src/DNIsolucionSCORM.java
@@ -5,6 +5,7 @@ public class DNIsolucionSCORM {
 DUDAS
 1- No seria mejor tener el metodo MAIN, al principio del codigo? en este ejemplo aparece antes el metodo del DNI
 2- En metodo LETRADNI, como sabe el MAIN que numeroDNI(del otro metodo) es el DNI introducido por Scanner?
+3- En un repositorio creado hace poco no me sale idea, out y src, unicamente aparece external libraries, y scratches and Console
 */
     public static char letraDNI(int numeroDNI) {
         char[] letras={'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B',
