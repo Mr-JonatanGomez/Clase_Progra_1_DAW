bf98eafcf85f9f662cfdc47c925f6b856b300fdd
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-01-19 13:28:46 +0100

    Act

--- Arrays_Multi/src/E6ConBorja.java
+++ Arrays_Multi/src/E6ConBorja.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class E6ConBorja {
     /*   (GeneradorPalabras) Crea un programa que cumpla los siguientes requisitos:
             x   1- Crear un de chars con todas las letras del abecedario.
@@ -7,27 +9,42 @@ public class E6ConBorja {
             - Imprimir por pantalla las palabras generadas
 
 */
-
+    static char[] letras;
 
     public static char[] generarAbecedario() {
-        char[] letras = new char[]{'A', 'E', 'I', 'O', 'U'};
+        char[] letras = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q',
+                'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
         return letras;
+
     }
 
-    public static String generarPalabras(char[] l, int longitud) {
-        String palabra = "" ; // declarar una palabra sin valor, tambien String palabra = new String("")
+    public static String generarPalabras(char[] l, int longitud) {// l es una referencia que necesita un array
+        // para nº palabras y longitud de cada una
+        String palabra = ""; // declarar una palabra sin valor, tambien String palabra = new String("")
+
         for (int i = 0; i < longitud; i++) {
-            char letra = l[(int) (Math.random() * longitud)];
+            char letra = l[(int) (Math.random() * l.length)];//( si pusieramos l[i]) genera letras en orden
             palabra += letra;
         }
         return palabra;
     }
 
-   /* public static String mostrar(String palabras){
-
-    }*/
 
     public static void main(String[] args) {
+        System.out.println("introduce cuantas letras quieres en tu palabra");
+        Scanner sc = new Scanner(System.in);
+        int longitudPalabra = sc.nextInt();
+        String palabraGenerada = generarPalabras(generarAbecedario(), longitudPalabra);//el array que necesita generarPalabras es generar abecedario, y ahora la longitud de las palabras
+        System.out.println(palabraGenerada);
+
+        System.out.println("Vamos a generar palabras aleatorias con la cantidad de letras aleatorias METIDAS EN JAVA 0-10, Cuantas quieres?");
+        int cuantas = sc.nextInt();
+        //System.out.println("Vamos a generar palabras aleatorias, Cuantas quieres?");
+        //int longCuantas= sc.nextInt();
+        for (int i = 0; i < cuantas; i++) {
+            palabraGenerada = generarPalabras(generarAbecedario(),(int)(Math.random()*10));
+            System.out.print(palabraGenerada+", ");
+        }
 
     }
 
