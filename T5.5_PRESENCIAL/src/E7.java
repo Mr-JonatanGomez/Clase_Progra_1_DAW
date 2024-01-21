c9b5338607d00af56fd08e386c433bb40e3d5326
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-01-21 18:21:19 +0100

    act

---
+++ T5.5_PRESENCIAL/src/E7.java
@@ -0,0 +1,7 @@
+public class E7 {
+    /*Escribe un programa que rellene un array de 100 elementos con números enteros aleatorios comprendidos
+entre 0 y 500 (ambos incluidos). A continuación el programa mostrará el array y preguntará si el usuario
+quiere destacar el máximo o el mínimo. Seguidamente se volverá a mostrar el array escribiendo el número
+destacado entre dobles asteriscos.
+Nota: para generar los números aleatorios podemos utilizar n = (int)(Math.random() * 501);*/
+}
