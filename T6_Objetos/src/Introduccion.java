2f70ba9442b2cb731ac76f4835c5195f7f779571
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-01-25 10:42:27 +0100

    act

--- T6_Objetos/src/Introduccion.java
+++ T6_Objetos/src/Introduccion.java
@@ -1,5 +1,7 @@
 import model.Usuario;
 
+import java.util.Scanner;
+
 public class Introduccion {
     public static void main(String[] args) {
         //primitiva -> ocupa poca memoria, solo guarda un dato
@@ -12,12 +14,31 @@ public class Introduccion {
         Usuario usuario2 = new Usuario ("Ferrari","TestaRosa","2458","gggg@sdfsf");
         // nombre, dni, perfil, altura, correo, telefono
         // hasta que el constructor no igualas sus cualidades  los valores son null
-
         Usuario usuario0= new Usuario();
+        Usuario usuario4= new Usuario("Calimero",652951);
+        Usuario usuario3= new Usuario("Liam","Gomez","j200001","Liam@mail",6529511);
 
-        System.out.println(usuario1.getNombre());
+
+
+        /*Para imprimir, necesitamos setter y getter, porque el objeto es privado,
+        esto se hace en el objeto
+        * */
+        Scanner sc = new Scanner(System.in);
+        System.out.println(usuario1.getNombre());//asi se imprime con el get
         usuario1.setNombre("Felipe");// cambia el nombre a usuario1
         System.out.println(usuario1.getNombre());
+        System.out.println(usuario2.getNombre());
+
+        System.out.println(usuario0.getNombre());
+        System.out.println(usuario3.getNombre());
+        System.out.println(usuario4.getNombre());
+
+        // usamos el saludo
+        usuario4.saludar("'Bienvenidos al apasionante mundo de la PROGRAMACION'");
+        //usamos la accion mostrar datos
+        usuario1.mostrarDatos();
+
+
 
     }
 }
--- T6_Objetos/src/model/Usuario.java
+++ T6_Objetos/src/model/Usuario.java
@@ -8,6 +8,8 @@ public class Usuario {
     // nombre, dni, perfil, altura, correo, telefono
     // y se colocan primero, todos son privados(por el encapsulamiento), solo el mismo puede ver sus cualidades
 
+
+    // para dar por defecto a todos una contraseña. root1234 se pone el valor en el constructor
     private String nombre, apellido, dni, correo, password;
     private int telefono;
 
@@ -24,26 +26,59 @@ public class Usuario {
 
     public Usuario(){}// recomendable TENER CONSTRUCTOR VACIO
 
-    public Usuario(String nombre1, String apellido1, String dni1){
+    public Usuario(String nombre, String apellido, String dni){
         // si no metemos estas igualaciones, en el main luego aunque este puesto el nombre sigue sin hacer efecto
-        this.nombre = nombre1;
-        this.apellido = apellido1;
-        this.dni=dni1;
+        this.nombre = nombre; // con el this.nombre coge String de clase, = nombre (referenciado)
+        this.apellido = apellido;
+        this.dni=dni;
+        this.password= "root1234";
     }
     public Usuario (String nombre, String apellido, String dni, String correo){
         this.nombre=nombre;
         this.apellido=apellido;
         this.dni=dni;
         this.correo=correo;
+    }
+    public Usuario (String nombre, String apellido, String dni, String correo, int telefono){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.dni=dni;
+        this.correo=correo;
+        this.telefono=telefono;
+    }
+    public Usuario (String nombre, int telefono){
+        this.nombre=nombre;
+        this.telefono=telefono;
+    }
+    // cosas que puede hacer un usuario
+    public void saludar(String mensaje){
+        System.out.println("El user con nombre "+this.nombre+" te está diciendo "+mensaje);
+    }
 
+    public void mostrarDatos(){
+        System.out.println("Nombre: "+this.nombre);// o nombre sin this, cuando acostumbremos
+        System.out.println("Apellido: "+apellido);
+        System.out.println("DNI: "+dni);
+        System.out.println("Correo: "+correo);
+        System.out.println("Password: "+password);
+        System.out.println("Telefono: "+telefono);
     }
 
     //settet y getter (para hacer de modo indirecto las cosas) 1 por cada variable
-    //setter cambia el valor de una variable (private-pubñic)
+    //setter cambia el valor de una variable (private-public)
     //getter lo da
 
 
-    // metiendo GET o SET se escribe casi solo
+    // metiendo GET o SET se escribe casi solo (get lleva el tipo, set solo void)
+    /*
+    se puede construir automaticamente mediante seleccion por atajo
+    ALT+INS elegimos getter&setter y selecciono todos, o los que quiera
+
+    tambien se puede construir el objeto seleccionando constructor, y todas las cualidades
+    (atributos)que pusimos al principio, primero generamos el none, para hacer el objeto vacio
+
+
+    */
     public String getNombre(){
         return this.nombre;
     }
@@ -54,5 +89,9 @@ public class Usuario {
 
     public int getTelefono() {
         return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
     }
 }
