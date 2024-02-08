65e76f2ad96b7940d650c2be530fff7a9ee926b2
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-02-08 22:56:52 +0100

    act

--- T7/Academia_Borja/src/model/Alumno.java
+++ T7/Academia_Borja/src/model/Alumno.java
@@ -1,5 +1,48 @@
 package model;
 
 public class Alumno {
+/*- Tenga tres atributos private de tipo Asignatura.
+- Tenga un constructor con tres parámetros de tipo Asignatura que inicialice los tres atributos.
+- Tenga un constructor con tres parámetros de tipo int que inicialice los tres atributos.
+- Tenga un getter para cada uno de los atributos.*/
+    private String nombre;
+    private Asignatura asignatura1, asignatura2, asignatura3;
 
+    public Asignatura (String nombre, ){}
+
+
+
+    //GETTER Y SETTER
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Asignatura getAsignatura1() {
+        return asignatura1;
+    }
+
+    public void setAsignatura1(Asignatura asignatura1) {
+        this.asignatura1 = asignatura1;
+    }
+
+    public Asignatura getAsignatura2() {
+        return asignatura2;
+    }
+
+    public void setAsignatura2(Asignatura asignatura2) {
+        this.asignatura2 = asignatura2;
+    }
+
+    public Asignatura getAsignatura3() {
+        return asignatura3;
+    }
+
+    public void setAsignatura3(Asignatura asignatura3) {
+        this.asignatura3 = asignatura3;
+    }
 }
