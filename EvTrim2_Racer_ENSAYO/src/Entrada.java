def1821ee21382d4e432d0397cc86e9b77e5700c
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-02-24 00:37:44 +0100

    Actualizacion

--- EvTrim2_Racer_ENSAYO/src/Entrada.java
+++ EvTrim2_Racer_ENSAYO/src/Entrada.java
@@ -9,15 +9,15 @@ public class Entrada {
     public static void main(String[] args) {
 
         //CREACION DE COCHES
-        Coche coche1 = new Coche("Citroen", "Xsara", "\uD83D\uDE99 C.McRae_1", 80);
-        Coche coche2 = new Coche("Hyundai", "i30", "\uD83D\uDE97 M.Villanueva_2", 110);
-        Coche coche3 = new Coche("Seat", "Panda", "\uD83D\uDE99 M.Fibonnacci_3", 30);
+        Coche coche1 = new Coche("Citroen", "Xsara", "\uD83D\uDE99 C.McRae", 80);
+        Coche coche2 = new Coche("Hyundai", "i30", "\uD83D\uDE97 M.Villanueva", 110);
+        Coche coche3 = new Coche("Seat", "Panda", "\uD83D\uDE99 M.Fibonnacci", 30);
         Coche coche4 = new Coche("Citroen", "2CV_Trucado", "\uD83C\uDFCE\uFE0F B.Martin_4", 201);
-        Coche coche5 = new Coche("Toyota", "Corolla", "\uD83D\uDE97 A.Lisbeth_5", 150);
-        Coche coche6 = new Coche("Subaru", "Impreza", "\uD83C\uDFCE\uFE0F J.Gomez_6", 221);
-        Coche coche7 = new Coche("Opel", "Corsa", "\uD83D\uDE97 V.Bullido_7", 162);
-        Coche coche8 = new Coche("Renault", "5 CopaTurbo", "\uD83D\uDE99 J.Ventura_8", 70);
-        Coche coche9 = new Coche("Subaru", "Impreza", "\uD83C\uDFCE\uFE0F E.AfanRivera_9", 275);
+        Coche coche5 = new Coche("Toyota", "Corolla", "\uD83D\uDE97 A.Lisbeth", 150);
+        Coche coche6 = new Coche("Subaru", "Impreza", "\uD83C\uDFCE\uFE0F J.Gomez", 221);
+        Coche coche7 = new Coche("Opel", "Corsa", "\uD83D\uDE97 V.Bullido", 162);
+        Coche coche8 = new Coche("Renault", "5 CopaTurbo", "\uD83D\uDE99 J.Ventura", 70);
+        Coche coche9 = new Coche("Subaru", "Impreza", "\uD83C\uDFCE\uFE0F E.AfanRivera", 275);
 
 
         Competicion mundial = new Competicion();//ya estan añadidas las carreras
