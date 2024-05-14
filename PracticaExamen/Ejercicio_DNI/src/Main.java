40c6f5541cc7397bedee3dbd89ceadb4d7236dc6
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-05-14 19:01:06 +0200

    ej_DNI

--- PracticaExamen/Ejercicio_DNI/src/Main.java
+++ PracticaExamen/Ejercicio_DNI/src/Main.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -15,37 +18,26 @@ public class Main {
         T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E
         */
         Scanner sc = new Scanner(System.in);
-        /*
-        int dni = -1;
-        int resto = -1;
-        char letra;
-
-        String[] letrasDNI = new String[]{"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B",
-                "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"};
-
-        System.out.println("introduce un numero de DNI para calcular la letra");
-        dni = sc.nextInt();
-        resto = dni % 23;
-
-        System.out.println(resto);
-        System.out.println(letrasDNI[resto]);
-*/
+      /*
         System.out.println("introduce tu dni sin letra");
-        int dni=sc.nextInt();
+        int dni = sc.nextInt();
 
         generarLetra(dni);
 
         System.out.println("PARTE 2 introduce tu dni sin letra");
-        int dni2=sc.nextInt();
+        int dni2 = sc.nextInt();
         System.out.println("introduce la letra que crees que tiene el dni y verifica");
-        String letra=sc.next();
-        introduceDNIyLetraVerSiEsCorrecto(dni2,letra);
+        String letra = sc.next();
+        introduceDNIyLetraVerSiEsCorrecto(dni2, letra);
+
+       */
+        ordenando();
     }
 
-    public static String generarLetra(int dni){
+    public static String generarLetra(int dni) {
         Scanner sc = new Scanner(System.in);
         int resto = -1;
-        String letraTuya=null;
+        String letraTuya = null;
 
 
         String[] letrasDNI = new String[]{"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B",
@@ -54,13 +46,14 @@ public class Main {
         resto = dni % 23;
 
         //System.out.println(resto);
-        letraTuya=(letrasDNI[resto]);
+        letraTuya = (letrasDNI[resto]);
         System.out.println(letraTuya);
         return letraTuya;
     }
-    public static void introduceDNIyLetraVerSiEsCorrecto(int dni, String letra){
+
+    public static void introduceDNIyLetraVerSiEsCorrecto(int dni, String letra) {
         int resto = -1;
-        String letraTuya=null;
+        String letraTuya = null;
 
 
         String[] letrasDNI = new String[]{"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B",
@@ -70,17 +63,49 @@ public class Main {
 
 
         //System.out.println(resto);
-        letraTuya=(letrasDNI[resto]);
+        letraTuya = (letrasDNI[resto]);
 
-        if (letraTuya.equalsIgnoreCase(letra)){
+        if (letraTuya.equalsIgnoreCase(letra)) {
             System.out.println("Has letra del DNI es correcto");
-        } else{
-            System.out.println("La letra es incorrecta, para este dni "+dni+" es la letra: "+letraTuya);
-            System.out.println("La letra que buscabas es"+letraTuya);
+        } else {
+            System.out.println("La letra es incorrecta, para este dni " + dni + " es la letra: " + letraTuya);
+            System.out.println("La letra que buscabas es" + letraTuya);
         }
 
     }
 
+    public static void ordenando(){
+        int [] posicion = new int[]{0,1,2,3,4,5,6,7,8,9};
+        ArrayList <Integer> posiciones = new ArrayList<>();
+        posiciones.add(4);
+        posiciones.add(7);
+        posiciones.add(6);
+        posiciones.add(10);
+        posiciones.add(5);
+        posiciones.add(8);
+        posiciones.add(2);
+        posiciones.add(1);
+        posiciones.add(3);
+        posiciones.add(9);
+        posiciones.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o1 > o2){
+                    return 1;
+                } else if (o1 < o2){
+                    return -1;
+                } else{
+                    return 0;
+                }
+            }
+        });
 
+        System.out.print(posiciones);
+
+        System.out.println("ARRAY SACANDO POSICION desde que i=3 hasta final");
+        for (int i = 3; i < posicion.length; i++) {
+            System.out.println(i);
+        }
+    }
 }
 
